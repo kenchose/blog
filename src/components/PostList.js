fdb788769895762c09ecedcf670d391391701620
Kenny Pham <kennypham@kennys-mbp-2.attlocal.net>
2021-04-07 02:04:07 -0700

    fetchUser action cretor and usersReducer created. UserHeader is created to increase our reusibility.

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,6 +3,8 @@ import React from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
 
+import UserHeader from "./UserHeader";
+
 class PostList extends React.Component {
 	componentDidMount() {
 		this.props.fetchPosts(); // when comp is rendered this action creator is called
@@ -18,6 +20,7 @@ class PostList extends React.Component {
 							<h2>{post.title}</h2>
 							<p>{post.body}</p>
 						</div>
+						<UserHeader userId={post.userId} />
 					</div>
 				</div>
 			);
